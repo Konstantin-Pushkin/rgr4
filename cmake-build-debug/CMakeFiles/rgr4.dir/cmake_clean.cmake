7f5ed7f3977515cf912d420189b25ac4ac091acf
Kostya <kostya@MacBook-Air.local>
2025-01-08 23:24:45 +0300

    pered

---
+++ cmake-build-debug/CMakeFiles/rgr4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rgr4.dir/DArray.cpp.o"
+  "CMakeFiles/rgr4.dir/DArray.cpp.o.d"
+  "rgr4"
+  "rgr4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgr4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
